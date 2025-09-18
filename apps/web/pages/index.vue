79c3120f35a28cf8c1c7ca1868ccfa15228ad842
Scott <scottmsandie@gmail.com>
2025-09-18 16:57:25 -0400

    Create index.vue

---
+++ apps/web/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-8">
+    <h1 class="text-2xl font-bold">Welcome to StackQuotes</h1>
+    <NuxtLink to="/dashboard" class="btn mt-4">Go to Dashboard</NuxtLink>
+  </div>
+</template>
