a39e6e5c66df28e0986c6b45577ac1a7353145b4
Scott <scottmsandie@gmail.com>
2025-09-18 20:12:44 -0400

    Update tailwind.config.ts

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,34 +1,15 @@
 import type { Config } from 'tailwindcss'
 
-
-export default <Partial<Config>>{
-content: [
-'./components/**/*.{vue,js,ts}',
-'./layouts/**/*.{vue,js,ts}',
-'./pages/**/*.{vue,js,ts}',
-'./plugins/**/*.{js,ts}',
-'./app.vue'
-],
-theme: {
-extend: {
-colors: {
-header: '#1E293B',
-bg: '#F8FAFC',
-text: '#64748B',
-cta: '#10B981',
-warn: '#F97316'
-}
-}
-}
-}
-```
-
-
-`apps/web/postcss.config.cjs`
-```js
-module.exports = {
-plugins: {
-tailwindcss: {},
-autoprefixer: {}
-}
-}
+export default {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
