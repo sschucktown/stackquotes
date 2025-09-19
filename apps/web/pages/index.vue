e3540f42d0f8be35d5aaf9c8091442e19a8957f0
Scott <scottmsandie@gmail.com>
2025-09-18 20:38:40 -0400

    Update index.vue

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,9 +1,3 @@
-<template>
-  <div class="p-8">
-    <h1 class="text-2xl font-bold">Welcome to StackQuotes</h1>
-    <NuxtLink to="/dashboard" class="btn mt-4">Go to Dashboard</NuxtLink>
-  </div>
-</template>
 <script setup>
 navigateTo('/dashboard')
 </script>
