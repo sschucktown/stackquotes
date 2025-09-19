661fcbfe740ac047dd4262b5f9987b203769ce16
Scott <scottmsandie@gmail.com>
2025-09-18 22:17:14 -0400

    Update index.vue

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,3 +1,8 @@
 <script setup>
+  const config = useRuntimeConfig()
+if (process.client) {
+  console.log('Supabase URL:', config.public.supabaseUrl)
+  console.log('Supabase Key exists?', !!config.public.supabaseAnonKey)
+}
 navigateTo('/dashboard')
 </script>
