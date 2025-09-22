f19c134d2469e6d03c7b3927ea20efb033f71f5a
Scott <scottmsandie@gmail.com>
2025-09-22 16:00:19 -0400

    Update useSb.ts

--- apps/web/composables/useSb.ts
+++ apps/web/composables/useSb.ts
@@ -1,6 +1,4 @@
 // apps/web/composables/useSb.ts
 export const useSb = () => {
-  const { $supabase } = useNuxtApp()
-  if (!$supabase) throw new Error('Supabase client not found')
-  return $supabase
+  return useSupabaseClient()
 }
