135953579ece432d1f748fd92429515ba8d3fb29
Scott <scottmsandie@gmail.com>
2025-09-23 18:13:02 -0400

    Update page.tsx

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,1 +1,9 @@
 
+export default function PrivacyPage() {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
+      <p>Our privacy policy will be published here soon.</p>
+    </div>
+  )
+}
