785f61740f34a62ab4ce51829c01ec88f03ee706
Scott Sandie <scottmsandie@gmail.com>
2025-10-23 16:03:40 -0400

    configstripe

---
+++ packages/config/src/stripePrices.js
@@ -0,0 +1,12 @@
+export const STRIPE_PRICES = {
+  FREE: "price_123free",
+  PRO: "price_456pro",
+  TEAM: "price_789team",
+};
+
+export const PRICE_ID_TO_PLAN = {
+  [STRIPE_PRICES.FREE]: "free",
+  [STRIPE_PRICES.PRO]: "pro",
+  [STRIPE_PRICES.TEAM]: "team",
+};
+
