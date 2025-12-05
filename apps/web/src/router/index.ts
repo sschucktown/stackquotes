5f91ba700900039733639c429e6d6475df521dda
Scott Sandie <scottmsandie@gmail.com>
2025-12-05 15:27:38 -0500

    index

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -135,6 +135,12 @@ const router = createRouter({
       meta: { public: true, allowAuthenticated: true },
     },
     {
+      path: "/prototype/contractor/messaging",
+      name: "ContractorMessagingInboxPrototype",
+      component: () => import("@/prototype/contractor/MessagingInbox.vue"),
+      meta: { public: true, allowAuthenticated: true },
+    },
+    {
       path: "/login",
       name: "login",
       component: () => import("@/pages/auth/LoginPage.vue"),
