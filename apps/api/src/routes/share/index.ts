f6ef1adceeadd8bac0b661d3430502e928d82b5a
Scott Sandie <scottmsandie@gmail.com>
2025-12-23 14:36:03 -0500

    asefd

--- apps/api/src/routes/share/index.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from "hono";
-import { proposalRouter } from "./proposal/index.js";
-
-export const shareRouter = new Hono();
-
-// /api/share/proposal/*
-shareRouter.route("/proposal", proposalRouter);
--- apps/api/src/routes/share/proposal/index.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from "hono";
-import { proposalTokenRouter } from "./[token]/index.js";
-
-export const proposalRouter = new Hono();
-
-// /api/share/proposal/:token/*
-proposalRouter.route("/:token", proposalTokenRouter);
